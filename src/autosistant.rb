1635c7e673e8f454a161d2aa34dff3c29bc3ce5a
Graeme Douglas <gdouglas.dev@gmail.com>
2012-09-29 19:30:16 -0700

    Add initial code base
    I have started writing the basic code for the project.  The main script is
    autosistant.rb.  config.rb  Stores both developer and coorporate configuration
    items.  chat.erb contains the chat application code.  All messages are sent
    via JQuery AJAX.

---
+++ src/autosistant.rb
@@ -0,0 +1,41 @@
+################################################################################
+# Author:		Graeme Douglas
+# Create date:		2012/09/29
+# Modify date/time:	2012/09/29/15:48
+##
+# The main execution script for the autosistant application.
+################################################################################
+
+### Requirements ###############################################################
+require 'rubygems'		# For Ruby version < 1.9
+require 'sinatra'		# Webframework to use
+
+require './config.rb'		# System wide configuration options
+if (DEBUG_ON)
+	require 'shotgun'	# Restart server on each page refresh
+end
+################################################################################
+
+### Routes #####################################################################
+get '/' do
+	"Hello, World"
+end
+
+get '/autosistant' do
+	erb :chat
+end
+
+post '/autosistant-ajax' do
+	"Message received! You sent: \"#{params[:message]}\""
+end
+
+get '/about' do
+	# TODO: Write this code!
+	"Graeme needs to write this code!"
+end
+
+not_found do
+	status 404
+	'Oops!  The page is not found!'
+end
+################################################################################
