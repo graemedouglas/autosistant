4c95595c4d72b238c4fdf9c098cfc65c7159320b
Graeme Douglas <gdouglas.dev@gmail.com>
2012-11-24 15:22:57 -0800

    Add project DFD, add more products

---
+++ src/sql/deep-products.sql
@@ -0,0 +1,151 @@
+-- Setup the product table.
+
+-- New products: Toyota.
+INSERT INTO product (name, price, qty)
+	VALUES ('Stock Wheels for 1980 Toyota Hilux', 100000, 14);
+INSERT INTO product (name, price, qty)
+	VALUES ('Brake Cable', 1000, 6);
+INSERT INTO product (name, price, qty)
+	VALUES ('1980 Toyota Hilux Windshield Wipers: Winter Edition', 1099, 8);
+INSERT INTO product (name, price, qty)
+	VALUES ('1980 Toyota Hilux Windshield Wipers: Summer Edition', 1099, 2);
+INSERT INTO product (name, price, qty)
+	VALUES ('1980 Toyota Hilux Rear Drum Brakes', 10909, 10);
+INSERT INTO product (name, price, qty)
+	VALUES ('1980 Toyota Hilux Front Disc Brakes', 7667, 4);
+INSERT INTO product (name, price, qty)
+	VALUES ('1980 Toyota Hilux Rear Drum Brakes', 8001, 7);
+INSERT INTO product (name, price, qty)
+	VALUES ('1980 Toyota MR2 Heavy Duty Winch', 60099, 0);
+INSERT INTO product (name, price, qty)
+	VALUES ('1985 Toyota MR2 Front Fender Set', 45000, 1);
+INSERT INTO product (name, price, qty)
+	VALUES ('1985 Toyota MR2 Front Brake Cable', 45000, 1);
+INSERT INTO product (name, price, qty)
+	VALUES ('Team Toyota Jacket', 19999, 23);
+
+-- New products: Honda.
+INSERT INTO product (name, price, qty)
+	VALUES ('Team Honda Jacket', 19999, 67);
+INSERT INTO product (name, price, qty)
+	VALUES ('1992 Honda Accord Stock Wheels', 80000, 3);
+INSERT INTO product (name, price, qty)
+	VALUES ('1992 Honda Accord Brake Cable', 2300, 70);
+INSERT INTO product (name, price, qty)
+	VALUES ('1992 Honda Accord Gas Pedal', 3001, 100);
+INSERT INTO product (name, price, qty)
+	VALUES ('1992 Honda Accord Brake Pedal', 3001, 99);
+INSERT INTO product (name, price, qty)
+	VALUES ('1992 Honda Accord Front Fenders', 34600, 0);
+INSERT INTO product (name, price, qty)
+	VALUES ('1992 Honda Accord Driver''s Side Floor Mat', 1997, 10);
+INSERT INTO product (name, price, qty)
+	VALUES ('1992 Hona Accord Exhaust Pipe', 20000, 5);
+INSERT INTO product (name, price, qty)
+	VALUES ('1992 Honda Accord Disc Brake Pads', 12003, 6);
+INSERT INTO product (name, price, qty)
+	VALUES ('1992 Honda Accord Door Lock set', 32000, 9);
+INSERT INTO product (name, price, qty)
+	VALUES ('1992 Honda Accord Winter Windshield Wipers', 1800, 1);
+INSERT INTO product (name, price, qty)
+	VALUES ('1992 Honda Accord Summer Windshield Wipers', 1800, 1);
+
+-- Chevrolet
+INSERT INTO product (name, price, qty)
+	VALUES ('Team Chevrolet Jacket', 20000, 9);
+INSERT INTO product (name, price, qty)
+	VALUES ('Chevrolet 350 Air Filter', 2100, 100);
+INSERT INTO product (name, price, qty)
+	VALUES ('1995 Chevrolet 2500HD Tailgate', 40000, 2);
+INSERT INTO product (name, price, qty)
+	VALUES ('1995 Chevrolet 2500HD Taillight Set', 12070, 2);
+INSERT INTO product (name, price, qty)
+	VALUES ('1995 Chevrolet 2500HD Fuel Injection System', 100050, 2);
+INSERT INTO product (name, price, qty)
+	VALUES ('1995 Chevrolet 2500HD Door Handle', 4998, 20);
+INSERT INTO product (name, price, qty)
+	VALUES ('1995 Chevrolet 2500HD Clutch Pedal', 7000, 1);
+INSERT INTO product (name, price, qty)
+	VALUES ('1995 Chevrolet 2500HD Brake Cable', 5000, 4);
+INSERT INTO product (name, price, qty)
+	VALUES ('1995 Chevrolet 2500HD Tailgate', 40000, 8);
+INSERT INTO product (name, price, qty)
+	VALUES ('1995 Chevrolet 2500HD Longbox Clutch Pedal', 7000, 1);
+INSERT INTO product (name, price, qty)
+	VALUES ('1995 Chevrolet 2500HD Longbox Brake Cable', 5000, 4);
+
+-- Ford
+INSERT INTO product (name, price, qty)
+	VALUES ('Team Ford Jacket', 20000, 0);
+INSERT INTO product (name, price, qty)
+	VALUES ('2012 Ford Mustang Spark Plugs', 3464, 400);
+INSERT INTO product (name, price, qty)
+	VALUES ('2012 Ford Mustang Brake Cable', 1972, 51);
+INSERT INTO product (name, price, qty)
+	VALUES ('2012 Ford Mustang Ignition Cables', 10040, 34);
+INSERT INTO product (name, price, qty)
+	VALUES ('2012 Ford Mustang Distributor', 20909, 23);
+INSERT INTO product (name, price, qty)
+	VALUES ('2013 Ford Mustang Headers', 47010, 6);
+INSERT INTO product (name, price, qty)
+	VALUES ('2013 Ford Mustang Spark Plugs', 3464, 212);
+INSERT INTO product (name, price, qty)
+	VALUES ('2013 Ford Mustang Brake Cable', 1972, 64);
+INSERT INTO product (name, price, qty)
+	VALUES ('2013 Ford Mustang Ignition Cables', 10040, 10);
+INSERT INTO product (name, price, qty)
+	VALUES ('2013 Ford Mustang Distributor', 20909, 32);
+INSERT INTO product (name, price, qty)
+	VALUES ('2013 Ford Mustang Headers', 47010, 11);
+
+-- Lamborghini
+INSERT INTO product (name, price, qty)
+	VALUES ('Team Lamborghini Jacket', 20000, 13);
+INSERT INTO product (name, price, qty)
+	VALUES ('2012 Aventador Brake Cable', 300000, 1);
+INSERT INTO product (name, price, qty)
+	VALUES ('2012 Aventador Gas Cap', 100000, 1);
+INSERT INTO product (name, price, qty)
+	VALUES ('2012 Aventador Clutch', 2000000, 1);
+INSERT INTO product (name, price, qty)
+	VALUES ('2012 Aventador Radiator', 2100000, 1);
+INSERT INTO product (name, price, qty)
+	VALUES ('2012 Aventador Steering Wheel', 400000, 1);
+INSERT INTO product (name, price, qty)
+	VALUES ('2012 Aventador Door', 100000, 1);
+INSERT INTO product (name, price, qty)
+	VALUES ('2012 Aventador Limited Edition Driving Gloves', 10000, 10);
+INSERT INTO product (name, price, qty)
+	VALUES ('2009 Murcielago Transmission Shifter', 50000, 1);
+INSERT INTO product (name, price, qty)
+	VALUES ('2009 Murcielago Track Tested Tires', 1000000, 2);
+INSERT INTO product (name, price, qty)
+	VALUES ('2009 Murcielago Professional Helmet', 95000, 4);
+
+-- Ferrari
+INSERT INTO product (name, price, qty)
+	VALUES ('Team Ferrari Jacket', 20000, 15);
+INSERT INTO product (name, price, qty)
+	VALUES ('Collector''s Team Ferrari Jacket', 200000, 2);
+INSERT INTO product (name, price, qty)
+	VALUES ('2003 Enzo Driving Lessons', 900000, 10000000);
+INSERT INTO product (name, price, qty)
+	VALUES ('2003 Enzo Sunglasses', 5000, 100);
+INSERT INTO product (name, price, qty)
+	VALUES ('2003 Enzo Formulated Oil - 1 Quart', 500000, 20);
+INSERT INTO product (name, price, qty)
+	VALUES ('2003 Enzo Brake Pad Set', 3000000, 1);
+INSERT INTO product (name, price, qty)
+	VALUES ('2012 458 Italia Seat Covers', 30000, 1);
+INSERT INTO product (name, price, qty)
+	VALUES ('2012 458 Italia Clutch', 2400000, 1);
+INSERT INTO product (name, price, qty)
+	VALUES ('2012 458 Italia RaceDay Radiator Fluid', 230000, 3);
+
+-- General products
+INSERT INTO product (name, price, qty)
+	VALUES ('Extroil 10W30 - 1 Quart', 1199, 130);
+INSERT INTO product (name, price, qty)
+	VALUES ('RoadRunners 10W30 - 1 Quart', 1203, 40);
+INSERT INTO product (name, price, qty)
+	VALUES ('MotoTown Windshield Fluid - 3 Litres', 2051, 87);
